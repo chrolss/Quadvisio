3dd518b01f3609860ace850ba29a2c938aba3665
chrolss <chrolss@kth.se>
2014-05-29 14:22:34 +0200

    lade till motor

--- Sensors/sensors.cpp
+++ Sensors/sensors.cpp
@@ -13,11 +13,20 @@ using namespace std;
 int main(int argc, char *argv[]){
 		
 	mpu6050 mpu;
-	for (int i = 0; i < 20; i++){
+	while (true){
         mpu.readSensorData();
         double x = mpu.getAccX();
         double y = mpu.getAccY();
         double z = mpu.getAccZ();
         std::cout<<"x:"<<x<<"\ty:"<<y<<"\tz:"<<z<<std::endl;
 	}
+/*
+
+	adxl345 adxl;
+	for (int i=0; i<20; i++){
+	adxl.readSensorData();
+	short x = adxl.getAccX();
+	std::cout << "x: " << x << std::endl;
+	}
+*/
 }
---
+++ motor/motorPWM.cpp
@@ -0,0 +1,1 @@
+// Quadvisio registered trademark awesomeness
