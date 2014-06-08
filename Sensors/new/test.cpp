478b9713c4feccf8c19a1b94fc7ea682b246ed3d
wynss <toniaxelsson@icloud.com>
2014-06-08 12:21:31 +0200

    Har börjat skriva om mpu6050 koden så den läsa all rådata

--- Sensors/new/test.cpp
+++ Sensors/new/test.cpp
@@ -13,42 +13,15 @@
 #include "mpu6050.h"
 #include <linux/i2c-dev.h>
 
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
 using namespace std;
 
-void setup() {
-    // initialize device
-    printf("Initializing I2C devices...\n");
-    
-    mpu6050 mpu;
-    
-    // verify connection
-    printf("Testing device connections...\n");
-    printf(mpu.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
-}
-
-void loop() {
-    // read raw accel/gyro measurements from device
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
-    
-    // display accel/gyro x/y/z values
-    printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",ax,ay,az,gx,gy,gz);
-}
 
 int main(int argc, const char * argv[])
 {
-    setup();
-    
-    for (;;)
-        loop();
-    
+    printf("Initializing I2C devices...\n");
+    mpu6050 mpu;
+    printf("Testing device connections...\n");
+    printf(mpu.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
     return 0;
 }
 
