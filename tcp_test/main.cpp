43a9c33ea7826073fa6f28c9b44f4578e5157675
wynss <toniaxelsson@icloud.com>
2014-06-30 17:52:33 +0200

    Kod för att testa tcp anslutningen

---
+++ tcp_test/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  SensorsHub
+//
+//  Created by Toni Axelsson on 2014-05-29.
+//  Copyright (c) 2014 Toni Axelsson. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include "communicate.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    Communicate c;
+    
+    return 0;
+}
+
