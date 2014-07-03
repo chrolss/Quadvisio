ee3a6ccd587ff6f7b8a449836a626fbf362883d9
wynss <toniaxelsson@icloud.com>
2014-07-03 21:10:32 +0200

    Städat upp lite

---
+++ SensorManager/SensorManager.cpp
@@ -0,0 +1,28 @@
+//
+//  SensorManager.cpp
+//  Atlas Software
+//
+//  Created by Quadvisio on 2014-06-06 as part of the Atlas Project.
+//  Copyright (c) 2014 Quadvisio. All rights reserved.
+//
+
+#include "SensorManager.h"
+
+SensorManager::SensorManager(){
+    mpu = new mpu6050(1, 0x68);
+}
+
+void SensorManager::initializeMPU(){
+    
+}
+
+void SensorManager::readMPU(float *input) {
+    input[0] = 1.0;
+    input[1] = 2.0;
+    input[2] = 3.0;
+    input[3] = 4.0;
+    input[4] = 5.0;
+    input[5] = 6.0;
+}
+
+
---
+++ SensorManager/mpu6050.h
@@ -0,0 +1,31 @@
+#ifndef MPU6050_H_
+#define MPU6050_H_
+
+class mpu6050 {
+
+private:
+    int I2CBus, I2CAdress
+    void convertAcc();
+    
+	uint8_t buf[14];
+	int fd;
+	char *fileName;
+    
+	int16_t accX, accY, accZ;
+	int16_t ax,ay,az,gx,gy,gz;
+    
+    double pitch, roll, yaw;
+    
+	void initialize();
+
+public:
+	mpu6050(int bus, int adress);
+	int8_t readRawMotion();
+    
+	double getAccX() {return accX;}
+	double getAccY() {return accY;}
+	double getAccZ() {return accZ;}
+	virtual ~mpu6050();
+};
+
+#endif /* MPU6050_H_ */
