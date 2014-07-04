123bf1a8948bbc8f9030be87e54a10d22cbfc44e
wynss <toniaxelsson@icloud.com>
2014-07-04 20:17:33 +0200

    Mapp för att testa Kontroll-klassen

---
+++ Controller/Controller.cpp
@@ -0,0 +1,22 @@
+//
+//  Controller.cpp
+//  Atlas Software
+//
+//  Created by Quadvisio on 2014-06-06 as part of the Atlas Project.
+//  Copyright (c) 2014 Quadvisio. All rights reserved.
+//
+
+#include "Controller.h"
+
+Controller::Controller(){
+    
+}
+
+void Controller::calcPWM(double *input, double *output) {
+    
+    output[0] = input[0];
+    output[1] = input[1];
+    output[2] = input[2];
+    output[3] = input[3];
+    
+}
---
+++ Controller/Controller.h
@@ -0,0 +1,24 @@
+//
+//  Controller.h
+//  Atlas Software
+//
+//  Created by Quadvisio on 2014-06-06 as part of the Atlas Project.
+//  Copyright (c) 2014 Quadvisio. All rights reserved.
+//
+
+#ifndef __Atlas__Controller__
+#define __Atlas__Controller__
+
+#include <iostream>
+
+class Controller{
+    
+public:
+    Controller();
+    void calcPWM(double *input, double *output);
+    
+private:
+    
+};
+
+#endif /* defined(__Atlas__Controller__) */
