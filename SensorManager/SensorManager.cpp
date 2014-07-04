6ddefb2a4752784e792f946b2bd2baa9a07264de
wynss <toniaxelsson@icloud.com>
2014-07-04 14:28:55 +0200

    testar

--- SensorManager/SensorManager.cpp
+++ SensorManager/SensorManager.cpp
@@ -16,11 +16,11 @@ void SensorManager::initializeMPU(){
     mpu->initialize();
 }
 
-void SensorManager::readMPU(int16_t *input) {
+void SensorManager::readMPU(double *input) {
     
     mpu->getMotion(&input[0], &input[1], &input[2], &input[3], &input[4], &input[5]);
     
-    printf("a/g: %6hd %6hd %6hd   %6hd %6hd %6hd\n",input[0],input[1],input[2],input[3],input[4],input[5]);
+    printf("a/g: %f %f %f   %f %f %f\n",input[0],input[1],input[2],input[3],input[4],input[5]);
 }
 
 
--- SensorManager/testRaw.cpp
+++ SensorManager/testRaw.cpp
@@ -3,7 +3,7 @@
 #include "SensorManager.h"
 #include "I2Cdev.h"
 
-int16_t input[6];
+double input[6];
 
 int main(int argc, const char * argv[])
 {
