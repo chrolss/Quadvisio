09fca493855754b9ca52a45783961cd44359f87c
chrolss <chrolss@kth.se>
2014-07-04 14:48:50 +0200

    tror mig ha hittat felet som fforhindrar oss att kora fler motorer samtidigt

--- motor/Motor.cpp
+++ motor/Motor.cpp
@@ -13,34 +13,30 @@ Motor::Motor(){
 }
 
 void Motor::initialize(){
+	
 	pwmLF = new BlackPWM(P8_13); //P8_13
-	pwmRF = new BlackPWM(P8_13); //P8_19
-	pwmLR = new BlackPWM(P9_14);	//P9_14
-	pwmRR = new BlackPWM(P9_21); //P9_21
 	sleep(1);
-	pwmLF->setRunState(run); 		//sätt igång signalen
-	pwmRF->setRunState(run);
-	pwmLR->setRunState(run);
-	pwmRR->setRunState(run);
+	pwmLF->setRunState(run); //sätt igång signalen
 	sleep(1);
-
-	pwmLF->setPeriodTime(5000000); 	//Definiera rätt period
+	pwmLF->setPeriodTime(5000000);
+	sleep(1);
+	pwmLF->setDutyPercent(0.0);
+	sleep(1);
+	pwmLF->setDutyPercent(20.0); //Speciellt för vår ESC
+	sleep(1);
+	printf("P8_13 ready\n");
+	
+	pwmRF = new BlackPWM(P8_19); //P8_19
+	sleep(1);
+	pwmRF->setRunState(run); //sätt igång signalen
+	sleep(1);
 	pwmRF->setPeriodTime(5000000);
-	pwmLR->setPeriodTime(5000000);
-	pwmRR->setPeriodTime(5000000);
 	sleep(1);
-	pwmLF->setDutyPercent(0.0);		//Sätt duty till 0 för initiering
 	pwmRF->setDutyPercent(0.0);
-	pwmLR->setDutyPercent(0.0);
-	pwmRR->setDutyPercent(0.0);
 	sleep(1);
-
-	pwmLF->setDutyPercent(20.0); 	//Speciellt för vår ESC, noll-nivån
-	pwmRF->setDutyPercent(20.0);
-	pwmLR->setDutyPercent(20.0);
-	pwmRR->setDutyPercent(20.0);
+	pwmRF->setDutyPercent(20.0); //Speciellt för vår ESC
 	sleep(1);
-	printf("PWM signal ready");
+	printf("P8_19 ready\n");
 }
 
 float Motor::mapper(float b){
@@ -51,8 +47,10 @@ float Motor::mapper(float b){
 void Motor::closePWM(){
 	pwmLF->setRunState(stop); //stäng alla fyra pwmportar
 	pwmRF->setRunState(stop);
+	/*
 	pwmLR->setRunState(stop);
 	pwmRR->setRunState(stop);
+	*/
 }
 
 void Motor::setPWM(float *output) {
@@ -61,8 +59,9 @@ void Motor::setPWM(float *output) {
 
     pwmLF->setDutyPercent(PWM[0]);
     pwmRF->setDutyPercent(PWM[1]);
+    /*
     pwmLR->setDutyPercent(PWM[2]);
     pwmRR->setDutyPercent(PWM[3]);
-    
+	*/
     //printf("The PWM values are: %f %f %f %f", PWM[0], PWM[1], PWM[2], PWM[3]);
 }
