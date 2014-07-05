3d31ed628fb31f4b73b028b2740ff4e2c3b1e3f1
chrolss <chrolss@kth.se>
2014-07-05 16:22:58 +0200

    testat massor, inget funkar, har mailat blacklibkillen och far snart se vad han sager

--- motor/superTest.cpp
+++ motor/superTest.cpp
@@ -7,36 +7,36 @@ float mapper(float b){
 	return val;
 }
 
-BlackPWM pwm1(P8_13);
-BlackPWM pwm2(P8_19);
+BlackPWM *pwm1 = new BlackPWM(P8_13);
+//BlackPWM *pwm2 = new BlackPWM(P8_19);
 
 float val;
 
 int main(){
-	pwm1.setRunState(run);
-	pwm2.setRunState(run);
+	pwm1->setRunState(run);
+	//pwm2->setRunState(run);
 	sleep(1);
-	pwm1.setPeriodTime(5000000);
-	pwm2.setPeriodTime(5000000);
+	pwm1->setPeriodTime(5000000);
+	//pwm2->setPeriodTime(5000000);
 	sleep(1);
-	pwm1.setDutyPercent(0.0);
-	pwm2.setDutyPercent(0.0);
+	pwm1->setDutyPercent(0.0);
+	//pwm2->setDutyPercent(0.0);
 	sleep(1);
-	pwm1.setDutyPercent(20.0);
-	pwm2.setDutyPercent(20.0); //Speciellt för vår ESC
+	pwm1->setDutyPercent(20.0);
+	//pwm2->setDutyPercent(20.0); //Speciellt för vår ESC
 	sleep(1);
 	while (true){
 		printf("Give pwm value 0 - 100\n");
 		std::cin >> val;
 		if (val > 100){
 			printf("Shutting down\n");
-			pwm1.setDutyPercent(20.0);
-			pwm2.setDutyPercent(20.0);
-			pwm1.setRunState(stop);
-			pwm2.setRunState(stop);
+			pwm1->setDutyPercent(20.0);
+			//pwm2->setDutyPercent(20.0);
+			pwm1->setRunState(stop);
+			//pwm2->setRunState(stop);
 			exit(1);
 		}
-		pwm1.setDutyPercent(mapper(val));
-		pwm2.setDutyPercent(mapper(val));
+		pwm1->setDutyPercent(mapper(val));
+		//pwm2->setDutyPercent(mapper(val));
 		}
 }
