214c00aff70354bd004d1b7596cde3edc6569806
chrolss <chrolss@kth.se>
2014-07-06 17:23:48 +0200

    testar vidare med python

--- py_motors/pwm.py
+++ py_motors/pwm.py
@@ -3,11 +3,12 @@ import time
 
 def initialize():
 	PWM.start("P8_13",20,200)
-	time.sleep(1)
+	time.sleep(0.5)
 	print('P8_13 initialized')
 	PWM.start("P9_14",20,200)
-	time.sleep(1)
+	time.sleep(0.5)
 	print('P9_14 initialized')
+	time.sleep(1)
 
 def set_pwm(a,b):
 	PWM.set_duty_cycle("P8_13",a)
@@ -16,7 +17,8 @@ def set_pwm(a,b):
 def close_pwm():
 	PWM.set_duty_cycle("P8_13",20)
 	PWM.set_duty_cycle("P9_14",20)
-	time.sleep(0.1)
+	time.sleep(1)
 	PWM.stop("P8_13")
 	PWM.stop("P9_14")
 	PWM.cleanup()
+	exit()
