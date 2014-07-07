56a4f19984579c342e83bdfb5c54ba58a2748549
chrolss <chrolss@kth.se>
2014-07-07 17:11:38 +0200

    Fyra motorer funkar finfint, och BBB verkar inte brinna.

--- py_motors/pwm.py
+++ py_motors/pwm.py
@@ -8,12 +8,12 @@ def initialize():
 	PWM.start("P9_14",20,200)
 	time.sleep(0.5)
 	print('P9_14 initialized')
-	PWM.start("P8_19",20,200)
+	PWM.start("P9_21",20,200)
 	time.sleep(0.5)
 	print('P8_19 initialized')
-	PWM.start("P9_16",20,200)
+	PWM.start("P9_42",20,200)
 	time.sleep(0.5)
-	print('P9_16 initialized')
+	print('P9_42 initialized')
 	time.sleep(2)
 
 def set_pwm1(a):
@@ -23,10 +23,10 @@ def set_pwm2(a):
 	PWM.set_duty_cycle("P9_14",a)
 	
 def set_pwm3(a):
-	PWM.set_duty_cycle("P8_19",a)
+	PWM.set_duty_cycle("P9_21",a)
 
 def set_pwm4(a):
-	PWM.set_duty_cycle("P9_16",a)
+	PWM.set_duty_cycle("P9_42",a)
 
 def set_pwm(a):
 	PWM.set_duty_cycle("P8_13",a)
@@ -36,12 +36,12 @@ def set_pwm(a):
 def close_pwm():
 	PWM.set_duty_cycle("P8_13",20)
 	PWM.set_duty_cycle("P9_14",20)
-	PWM.set_duty_cycle("P8_19",20)
-	PWM.set_duty_cycle("P9_16",20)
+	PWM.set_duty_cycle("P9_21",20)
+	PWM.set_duty_cycle("P9_42",20)
 	time.sleep(1)
 	PWM.stop("P8_13")
 	PWM.stop("P9_14")
-	PWM.stop("P8_19")
-	PWM.stop("P9_16")
+	PWM.stop("P9_21")
+	PWM.stop("P9_42")
 	PWM.cleanup()
 	exit()
