5ac88872b27cbc48f335144b1c6a7c6fa3fd99f9
wynss <toniaxelsson@icloud.com>
2014-07-07 21:08:25 +0200

    DMP funkar på mpun nu

--- SensorManager/SensorManager.cpp
+++ SensorManager/SensorManager.cpp
@@ -83,6 +83,7 @@ void SensorManager::readDMP(double *input) {
     mpu->dmpGetYawPitchRoll(ypr, &q, &gravity);
     printf("ypr  %7.2f %7.2f %7.2f    ", ypr[0] * 180/M_PI, ypr[1] * 180/M_PI, ypr[2] * 180/M_PI);
     
+    printf("\n");
     
     // display initial world-frame acceleration, adjusted to remove gravity
     // and rotated based on known orientation from quaternion
