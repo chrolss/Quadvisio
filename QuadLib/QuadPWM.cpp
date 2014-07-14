a0254e4b0188cf115ea3f1bebfa290f976c99a3a
wynss <toniaxelsson@icloud.com>
2014-07-14 21:36:34 +0200

    Funkar att sätta ingång en signal och få motorn att snurra

--- QuadLib/QuadPWM.cpp
+++ QuadLib/QuadPWM.cpp
@@ -62,16 +62,16 @@ void QuadPWM::intitialize(std::string pwmName) {
 
     sleep(1);
     
-    printf("Setting polarity to 0\n");
+    printf("Starting %s\n", newPwm.name.c_str());
     setPolarity(newPwm.name, 0);
     
     sleep(1);
     
-    printf("Setting period to 2000000\n");
+    //printf("Setting period to 2000000\n");
     setPeriod(newPwm.name, 2000000);
     
     sleep(1);
-    printf("Setting duty to 1000000\n");
+    //printf("Setting duty to 1000000\n");
     setDuty(newPwm.name, 1000000);
     
     /////////////////////////////////////////
