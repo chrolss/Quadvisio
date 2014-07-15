361e5f6835ced4ee23fc6ac79e9990535df3e863
wynss <toniaxelsson@icloud.com>
2014-07-15 16:47:05 +0200

    Fler motor test redo

--- QuadLib/test.cpp
+++ QuadLib/test.cpp
@@ -18,16 +18,22 @@ int main(int argc, const char * argv[]) {
     QuadPWM pwm;
     pwm.intitialize("P8_13");
     pwm.intitialize("P9_14");
+    pwm.intitialize("P9_21");
+    pwm.intitialize("P9_42");
     
     sleep(2);
     std::cout << "Setting duty to 1200000" << std::endl;
     pwm.setDuty("P8_13", 1200000);
     pwm.setDuty("P9_14", 1200000);
+    pwm.setDuty("P9_21", 1200000);
+    pwm.setDuty("P9_42", 1200000);
     
     sleep(4);
     std::cout << "Setting duty to 1000000" << std::endl;
     pwm.setDuty("P8_13", 1000000);
     pwm.setDuty("P9_14", 1000000);
+    pwm.setDuty("P9_21", 1000000);
+    pwm.setDuty("P9_42", 1000000);
 
     return 0;
 }
