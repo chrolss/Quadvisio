e839d389e089148fc5e47f4fff76dbedc319dca4
wynss <toniaxelsson@icloud.com>
2014-10-17 17:05:55 +0200

    Tester

--- CameraManager/CameraManager.cpp
+++ CameraManager/CameraManager.cpp
@@ -168,47 +168,25 @@ void CameraManager::start_capturing() {
     unsigned int i;
     enum v4l2_buf_type type;
     
-    switch (io) {
-        case IO_METHOD_READ:
-            /* Nothing to do. */
-            break;
-            
-        case IO_METHOD_MMAP:
-            for (i = 0; i < n_buffers; ++i) {
-                struct v4l2_buffer buf;
-                
-                CLEAR(buf);
-                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory = V4L2_MEMORY_MMAP;
-                buf.index = i;
-                
-                if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
-                    perror("VIDIOC_QBUF");
-            }
-            type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
-                perror("VIDIOC_STREAMON");
-            break;
-            
-        case IO_METHOD_USERPTR:
-            for (i = 0; i < n_buffers; ++i) {
-                struct v4l2_buffer buf;
-                
-                CLEAR(buf);
-                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-                buf.memory = V4L2_MEMORY_USERPTR;
-                buf.index = i;
-                buf.m.userptr = (unsigned long) buffers[i].start;
-                buf.length = buffers[i].length;
-                
-                if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
-                    perror("VIDIOC_QBUF");
-            }
-            type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-            if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
-                perror("VIDIOC_STREAMON");
-            break;
+
+    for (i = 0; i < n_buffers; ++i) {
+        struct v4l2_buffer buf;
+        
+        CLEAR(buf);
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        
+        if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+            perror("VIDIOC_QBUF");
     }
+    
+    std::cout << "Stream on" << std::endl;
+
+    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
+        perror("VIDIOC_STREAMON");
+
 }
 
 void CameraManager::grab_frame() {
