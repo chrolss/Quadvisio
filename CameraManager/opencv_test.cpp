9f89ce400460cdac9bbccd01d36f37ad910b25e4
wynss <toniaxelsson@icloud.com>
2014-10-17 14:56:02 +0200

    Tester

---
+++ CameraManager/opencv_test.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  CameraManager
+//
+//  Created by Toni Axelsson on 2014-10-16.
+//  Copyright (c) 2014 Quadvisio. All rights reserved.
+//
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+    
+
+    Mat frame;
+    cap >> frame; // get a new frame from camera
+
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
