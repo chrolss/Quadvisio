79e77a772cc3c1803457c7f494e3414ca54dbfca
wynss <toniaxelsson@icloud.com>
2014-10-29 22:32:26 +0100

    Snart redo att testa

--- CameraManager2/test.cpp
+++ CameraManager2/test.cpp
@@ -7,35 +7,13 @@
 //
 
 #include <iostream>
-#include "CameraManager.h"
+#include "CameraManager2.h"
 
 int main(int argc, const char * argv[]) {
     
     std::cout << "Hej" << std::endl;
 
     CameraManager2 camManager;
-    
-    std::cout << "Starting camera" << std::endl;
-
-    if(camManager.initializeCamera(640,480)<0) {
-        printf("Error");
-    }
-    
-    
-    printf("Grabbing and saving frame\n");
-    camManager.grab_frame();
-    
-    /*
-    printf("Setting resolution to 640x480\n");
-    camManager.closeCamera();
-    camManager.initializeCamera(640, 480);
-    
-    printf("Grabbing and saving frame\n");
-    camManager.getImageBuffer();
-    */
-    
-    printf("Closing camera\n");
-    camManager.closeCamera();
     
     return 0;
 }
