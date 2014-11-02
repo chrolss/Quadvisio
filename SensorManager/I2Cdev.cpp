0b7783706a0513431d788aae636f3269a9692235
wynss <toniaxelsson@icloud.com>
2014-11-02 14:13:11 +0100

    Redo för test med mpu och bmp samtidigt

--- SensorManager/I2Cdev.cpp
+++ SensorManager/I2Cdev.cpp
@@ -41,7 +41,6 @@ THE SOFTWARE.
 ===============================================
 */
 
-#include <iostream>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -52,7 +51,6 @@ THE SOFTWARE.
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 #include "I2Cdev.h"
 
